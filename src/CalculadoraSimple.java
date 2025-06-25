7f39f86edc25ecdb45b4e75264c18f0f983b2f0f
Abi Rivas <abi.rivas193@std.uni.edu.ni>
2025-06-25 11:40:54 -0600

    cambios de abi en multiplicacion

--- src/CalculadoraSimple.java
+++ src/CalculadoraSimple.java
@@ -18,20 +18,10 @@ public class CalculadoraSimple {
         int resta = a - b;
         System.out.println("Resta: " + resta);
 
-<<<<<<< HEAD
-       
-
         //Multiplicacion
         int mult = a * b;
         System.out.println("Multiplicacion: " + mult);
 
          scanner.close();
-=======
-        // Estudiante a -abi multiplicacion-feature
-        int multiplicacion = a * b;
-        System.out.println("la multiplicacion: "+ multiplicacion);
-
-        scanner.close();
->>>>>>> 133f495781a7631bfa06e34c20a0a630bcdb47fc
     }
 }
