b3d800299a9183c4a3cdb8779009d808dd3400d3
JackDominguez <jack.gutierrez745@std.uni.edu.ni>
2025-06-25 12:03:10 -0600

    Agrega archivo nuevo a la rama main

---
+++ JAVABASE/src/CalculadoraSimple.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CalculadoraSimple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número:");
+        int a = scanner.nextInt();
+
+        System.out.println("Ingrese el segundo número:");
+        int b = scanner.nextInt();
+
+        // Suma
+        int suma = a + b;
+        System.out.println("Suma: " + suma);
+
+        // Resta
+        int resta = a - b;
+        System.out.println("Resta: " + resta);
+
+             //Division
+        if (a != 0 && b != 0) {
+            double division = (double) a / divisor;
+            System.out.println("División: " + division);
+        } else {
+            System.out.println("Error: No se puede dividir entre 0.");
+        }
+
+        scanner.close();
+
+        
+    }
+}
+
