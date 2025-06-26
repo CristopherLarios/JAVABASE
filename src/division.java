47613d76edadb4744b9ae4faf59a7020a83c7999
Susan Lacayo <susan.lacayo@std.uni.edu.ni>
2025-06-25 22:51:29 -0600

    funcionalidad 2

---
+++ src/division.java
@@ -0,0 +1,15 @@
+public class division {
+    
+    public static void main(String[]args){
+    int a = 10;
+int b = 2;
+
+if (b != 0) {
+    int division = a / b;
+    System.out.println("La división es: " + division);
+} else {
+    System.out.println("No se puede dividir entre 0.");
+}
+    }
+}
+
